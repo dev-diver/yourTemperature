1db6994fed4616d7b703216305f5085205fa4db5
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2023-10-12 11:48:39 +0900

    프론트 일부 수정 (#7)

--- app.py
+++ app.py
@@ -24,8 +24,13 @@ SECRET_KEY = 'SPARTA'
 bucket_name = 'krafton-yourname'
 bucket_url= f'https://{bucket_name}.s3.amazonaws.com'
 base_profile_url = f'{bucket_url}/baseprofile.png'
+base_state_url = f'{bucket_url}/basestate.png'
 
-s3_client = boto3.client('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=S3SECRET_KEY)
+s3_client = boto3.client(
+    's3', 
+    aws_access_key_id=ACCESS_KEY, 
+    aws_secret_access_key=S3SECRET_KEY
+)
 
 db=client.yourname
 
@@ -109,60 +114,81 @@ def api_login():
 
 @app.route('/api/vote', methods=['POST'])
 def vote():
-    state = request.form.get('state')
-    email = request.form.get('email')
-
-    user = db.login.find_one({'email',email})
-    nickname = user['nickname'] or '이름없음'
-    profile = user['profile'] or base_profile_url
-    message = request.form.get('message','')
-    timestamp = datetime.utcnow()
-    
-    vote = {
-        'state':state,
-        'email':email,
-        'nickname':nickname,
-        'profile_url':profile,
-        'message':message,
-        'timestamp':timestamp,
-    }
-    db.vote.insert_one(vote)
+    try:
+        state = request.form.get('state')
+        email = request.form.get('email')
+        user = getUserByEmail(email)
+        message = request.form.get('message','')
+        timestamp = datetime.utcnow()
+        
+        vote = {
+            'state':state,
+            'email':email,
+            'nickname':user['nickname'],
+            'profile':user['profile'],
+            'message':message,
+            'timestamp':timestamp,
+        }
+        db.vote.insert_one(vote)
+    except:
+        return jsonify({'result':'fail'})
     return jsonify({'result':'success'})
 
 @app.route('/api/votes', methods=['GET'])
 def votes():
-    lastSet = db.set.find_one(sort=[('timestamp', DESCENDING)])
-    lastTime = lastSet['timestamp']
-    recent_votes = list(db.vote.find({'timestamp': {'$gte': lastTime}}))
-    hot = [doc for doc in recent_votes if doc['state'] == 'hot']
-    good = [doc for doc in recent_votes if doc['state'] == 'good']
-    cold = [doc for doc in recent_votes if doc['state'] == 'cold']
-    states = {'hot':len(hot),'good':len(good),'cold':len(cold)}
-    max_state = max(states, key=lambda k: states[k])
-
-    response_data = {
+    data = {
+        'hot':[],
+        'good':[],
+        'cold':[],
+        'most':'none'
+    }
+    try:
+        lastSet = db.set.find_one(sort=[('timestamp', DESCENDING)])
+        lastTime = lastSet['timestamp']
+        recent_votes = list(db.vote.find({'timestamp': {'$gte': lastTime}}))
+        hot = [doc for doc in recent_votes if doc['state'] == 'hot']
+        good = [doc for doc in recent_votes if doc['state'] == 'good']
+        cold = [doc for doc in recent_votes if doc['state'] == 'cold']
+        states = {'hot':len(hot),'good':len(good),'cold':len(cold)}
+        max_state = max(states, key=lambda k: states[k])
+    except:
+        return jsonify({'result':'fail'}), 400
+    data = {
         'hot': hot,
         'good': good,
         'cold': cold,
         'most': max_state
     }
     result = {'result':'success'}
+    result.update(data)
     return jsonify(result)
 
 @app.route('/api/stateImages', methods=['GET'])
 def state_images():
-    state = request.args.get('state','none')
-    images = db.set.find({'state': state})
-    image_urls = [doc.url for doc in images]
+    image = {'img_url':base_state_url}
+    try:
+        state = request.args.get('state','none')
+        pipeline = [
+            {"$match": {"state": state}},
+            {"$sample": {"size": 1}}
+        ]
+        image = list(db.set.aggregate(pipeline))[0]
+        print(image['img_url'])
+    except:
+        jsonify({'result':'fail','image_url':image['img_url']})
     result = {'result':'success'}
-    return jsonify(result.update(image_urls))
+    image_url = {'image_url':image['img_url']}
+    result.update(image_url)
+    return jsonify(result)
 
 @app.route('/api/uploadImage', methods=['POST'])
 def upload_image():
-    email=request.form['email']
-    state=request.form['state']
+    email = request.form['email']
+    user = getUserByEmail(email)
+    nickname = user['nickname']
+    state = request.form['state']
     file = request.files['file']
-    category= request.form.get('category','state')
+    category = request.form.get('category','state')
     print(category)
     timestamp = get_js_timestamp()
     if file:
@@ -170,19 +196,24 @@ def upload_image():
         filename = category+'/'+state+str(timestamp)
         print(filename)
         # S3에 파일 업로드
-        s3_client.upload_fileobj(file, 'krafton-yourname', filename)
+        try:
+            s3_client.upload_fileobj(file, 'krafton-yourname', filename)
+        except:
+            print('이미지 서버 이상')
+            return jsonify({'result':'fail','message':'이미지 서버가 좋지 않습니다.'}), 400
         # S3 파일 URL 생성
         file_url = f"{bucket_url}/{filename}"
 
         doc = {
             'email':email,
+            'nickname':nickname,
             'state':state,
-            'img_url': file_url}
-        db.image.insert_one(doc)
-        
-        return jsonify({'result': 'success'}), 200
+            'img_url': file_url
+        }
+        db.image.insert_one(doc)   
     else:
-        return jsonify({'result': 'fail', 'error': 'No file uploaded'}), 400
+        return jsonify({'result': 'fail', 'message': '파일 업로드를 안했습니다.'}), 400
+    return jsonify({'result': 'success'}), 200
 
 @app.route('/api/set', methods=['POST'])
 def set_temperature():
@@ -191,14 +222,15 @@ def set_temperature():
 
     user = db.login.find_one({'email',email})
     nickname = user['nickname'] or '이름없음'
-    profile = user['profile'] or base_profile
+    profile = user['profile'] or base_profile_url
     timestamp = datetime.utcnow()
     set = {
         'email':email,
         'temperature':temperature,
         'nickname':nickname,
         'profile':profile,
-        'timestamp':timestamp}
+        'timestamp':timestamp
+    }
     db.set.insert_one(set)
     result = {'result':'success'}
     return jsonify(result)
@@ -209,6 +241,19 @@ def get_js_timestamp():
     unix_timestamp = now.timestamp()
     js_timestamp = int(unix_timestamp * 1000)
     return js_timestamp
+
+def getUserByEmail(email):
+    try:
+        userDoc = db.login.find_one({'email',email})
+    except:
+        print("해당 email 없음")
+    nickname = userDoc['nickname'] or '이름없음'
+    profile = userDoc['profile'] or base_profile_url
+    user = {
+        'nickname':nickname,
+        'profile':profile
+    }
+    return user
 
 if __name__ == '__main__':
     app.run(debug=True)
