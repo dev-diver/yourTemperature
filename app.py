a1d25ccd3acb0b32d72491f1ef46a9f6cd238de6
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2023-10-13 11:49:41 +0900

    로그인, 레지스터 bulma 적용

--- app.py
+++ app.py
@@ -362,5 +362,5 @@ def getUserByEmail(email):
     return user
 
 if __name__ == '__main__':
-    # app.run('0,0,0,0',port=5000,debug=True)
-    app.run(port=5000,debug=True)
+    app.run('0.0.0.0',port=5000,debug=True)
+    #app.run(port=5000,debug=True)
